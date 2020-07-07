7877b888335c512eba811c5bc7de208a83738a34
Haoru Xue <hxue@ucsd.edu>
2020-07-06 17:36:12 -0700

    Test speed and throttle together

--- Tests/test_serial_speed/test_serial_speed.py
+++ Tests/test_serial_speed/test_serial_speed.py
@@ -7,20 +7,39 @@ import time
 
 ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600)
 
-#Time the process
-start_time = time.time()
-# Send a request
-print('Sending request for speed')
-ser.write(b'speed\n')
-# Wait for return value
-speed = ser.read_until()
-#Stop the timer
-duration = time.time() - start_time
-print('Duration is:', duration, ', speed:', speed)
 
-start_time = time.time()
-ser.write(b'throttle\n')
-print('\nSending throttle request')
-throttle = ser.readline()
-duration = time.time() - start_time
-print('Duration is:', duration, ', throttle:', throttle)
+def test_seperately():
+    # Time the process
+    start_time = time.time()
+    # Send a request
+    print('Sending request for speed')
+    ser.write(b'speed\n')
+    # Wait for return value
+    speed = ser.read_until()
+    # Stop the timer
+    duration = time.time() - start_time
+    print('Duration is:', duration, ', speed:', speed)
+
+    start_time = time.time()
+    ser.write(b'throttle\n')
+    print('\nSending throttle request')
+    throttle = ser.readline()
+    duration = time.time() - start_time
+    print('Duration is:', duration, ', throttle:', throttle)
+
+
+def test_together():
+    # Time the process
+    start_time = time.time()
+    # Send request for speed and throttle
+    print('Sending request for speed')
+    ser.write(b'speed\n')
+    ser.write(b'throttle\n')
+    speed = ser.read_line()
+    throttle = ser.readline()
+    duration = time.time() - start_time
+    print('Duration is:', duration, ', speed:', speed, 'throttle:', throttle)
+
+
+# test_seperately()
+test_together()
