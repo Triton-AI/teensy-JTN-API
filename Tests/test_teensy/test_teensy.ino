ee3e90613bb5450e8d4fd0b6dfd26aa82efbc7cd
Haoru <hxue@ucsd.edu>
2020-09-28 10:51:56 -0700

    new api

--- Tests/test_teensy/test_teensy.ino
+++ Tests/test_teensy/test_teensy.ino
@@ -16,7 +16,7 @@ int escPin = 6;
 
 //boundary values
 int zero_throttle_pwm = 370;
-int center_steering = 450;
+int center_steering_pwm = 450;
 int max_throttle_pwm = 405;
 int min_throttle_pwm = 330;
 int min_steering_pwm = 335;
@@ -30,11 +30,25 @@ bool isSafeSteering(int steering_pwm){
   return steering_pwm >= min_steering_pwm && steering_pwm <= max_steering_pwm;
 }
 
-int mapPWM(float input, int max_pwm, int min_pwm){
+int mapPWM(float input, int max_pwm, int mid_pwm, int min_pwm){
   //Take input [-1,1] and map it to pwm
-  int center_pwm = 0;
-  center_pwm = (max_pwm + min_pwm) / 2;
-  return center_pwm + (int)((center_pwm - min_pwm) * input);
+
+  if (input == 0)
+    return mid_pwm;
+
+  else if (input < 0)
+    return (int)((mid_pwm) + (mid_pwm - min_pwm) * input);
+
+  else if (input > 0)
+    return (int)((mid_pwm) + (max_pwm - mid_pwm) * input);
+}
+
+int getIntAfter(String words, String subword){
+  return words.substring(words.indexOf(subword) + subword.length()).toInt();
+}
+
+bool stringContains(String words, String toMatch){
+  return words.indexOf(toMatch) >= 0;
 }
 
 void setup() {
@@ -46,7 +60,7 @@ void setup() {
   pinMode(escPin, OUTPUT);
 
   //initialize pwm to stable position
-  analogWrite(servoPin, center_steering);
+  analogWrite(servoPin, center_steering_pwm);
   analogWrite(escPin, zero_throttle_pwm);
 }
 
@@ -55,7 +69,7 @@ void loop() {
   while (Serial.available() == 0);
 
   //Read request
-  input = Serial.readStringUntil('\n');
+  input = Serial.readStringUntil('\n').toLowerCase();
 
   //Process input
   if (input.indexOf("poll") >= 0){
@@ -70,12 +84,13 @@ void loop() {
     }
       Serial.print('\n');
   }
+
   else if (input.indexOf("command") >= 0){
     int thr_pos = input.indexOf("throttle");
     int ste_pos = input.indexOf("steering");
     if (thr_pos >= 0){
       throttle = input.substring(thr_pos + 9).toFloat();
-      throttle_pwm = mapPWM(throttle, max_throttle_pwm, min_throttle_pwm);
+      throttle_pwm = mapPWM(throttle, max_throttle_pwm, zero_throttle_pwm ,min_throttle_pwm);
       if (isSafeThrottle(throttle_pwm))
         analogWrite(escPin, throttle_pwm);
        else
@@ -86,14 +101,51 @@ void loop() {
     }
     else if (ste_pos >= 0){
       steering = input.substring(ste_pos + 9).toFloat();
-      steering_pwm = mapPWM(throttle, max_steering_pwm, min_steering_pwm);
+      steering_pwm = mapPWM(steering, max_steering_pwm, center_steering_pwm, min_steering_pwm);
       if (isSafeSteering(steering_pwm))
         analogWrite(servoPin, steering_pwm);
        else
-        analogWrite(servoPin, center_steering);
+        analogWrite(servoPin, center_steering_pwm);
         
       //Serial.print("steering set to ");
       //Serial.print(steering);
     }
+    else if (stringContains(input, "shutdown")){
+      analogWrite(servoPin, center_steering_pwm);
+      analogWrite(escPin, zero_throttle_pwm);
+    }
+  }
+
+  else if (input.indexOf("try") >= 0){ //calibration process
+    int thr_pos = input.indexOf("throttle");
+    int ste_pos = input.indexOf("steering");
+    if (thr_pos >= 0){
+      throttle_pwm = input.substring(thr_pos + 9).toInt();
+      analogWrite(escPin, throttle_pwm);
+    }
+    else if (ste_pos >= 0){
+      steering_pwm = input.substring(ste_pos +9).toInt();
+      analogWrite(servoPin, steering_pwm);
+    }
+  }
+
+  else if (input.indexOf("calibrate") >= 0){
+    if (stringContains(input, "maxforward"))
+      max_throttle_pwm = getIntAfter(input, "_");
+
+    else if (stringContains(input, "maxreverse"))
+      min_throttle_pwm = getIntAfter(input, "_");
+
+    else if (stringContains(input, "neutralthrottle"))
+      zero_throttle_pwm = getIntAfter(input, "_");
+
+    else if (stringContains(input, "maxright"))
+      min_steering_pwm = getIntAfter(input, "_");
+
+    else if (stringContains(input, "maxleft"))
+      max_steering_pwm = getIntAfter(input, "_");
+
+    else if (stringContains(input, "neutralsteering"))
+      center_steering_pwm = getIntAfter(input, "_");
   }
 }
